9752b76c3f184c3452ae3b5b582f3d0f671fc10e
damray <damien.raynal@gmail.com>
2025-10-31 14:16:30 +0100

    Draft

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,            // écoute sur 0.0.0.0 (obligatoire en Docker)
+    port: 5173,            // même port que dans docker-compose
+    strictPort: true,      // évite qu’il choisisse un autre port si 5173 est pris
+    watch: {
+      usePolling: true,    // utile quand Docker n’envoie pas les événements inotify
+    },
+    hmr: {
+      host: 'localhost',   // adresse que ton navigateur utilise
+      port: 5173,          // port WebSocket pour le hot reload
+    },
   },
 });
